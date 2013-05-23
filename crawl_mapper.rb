3413c211d5c55d5fed223fc83750114e27fc371a
Margus Haavala <margus.haavala@gmail.com>
2013-05-23 23:31:04 +0300

    initial commit

---
+++ crawl_mapper.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'json'
+
+STDIN.each do |line|  
+  # Commoncrawl metadata file starts with url, followed by the tag character and metadata data in JSON format
+  # Page url is the key
+  key, value = line.chomp.split('\t', 2)
+  links = []
+  if value
+    begin
+      links = JSON.parse(value.to_s)['content']['links']
+    rescue JSON::ParserError => p
+      warn "JSON parse error: \n #{p.inspect}"
+    end
+  end
+  out_links = []
+  links.each do |l|
+    out_links <<  l['href'] if l['type']== 'link'
+  end
+  warn out_links.inspect
+  puts key + "\t" + out_links.uniq.join(' ')
+end
---
+++ crawl_reducer.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+warn "reduce.rb"
+ARGF.each_line do |line|
+  warn "working with line: #{line}"
+  line = line.chomp
+  key, value = line.split(/\t/)
+
+  if key and value
+	   puts key + "\t" + "1.0 " + value
+  end
+
+end
